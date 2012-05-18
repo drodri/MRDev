2e98c4b809155d2114b1de135379b6c8863aa378
Diego Rordriguez-Losada <diego.rlosada@upm.es>
2012-05-18 14:19:37 +0200

    localizer

--- libraries/localizer/localizer.h
+++ libraries/localizer/localizer.h
@@ -19,6 +19,9 @@ public:
 		filterTraj.setColor(0,0,255);
 		setOffset(Pose3D(0.1,0,0.4,0,0,0));
 		neffThreshold=0.75;
+		base=new Pioneer3ATSim(); 
+		base->remove((*base)[5]);base->remove((*base)[4]);base->remove((*base)[3]);base->remove((*base)[2]);
+		skipLaser=1;
 	}
 	//(0.1,0,0.4,0,0,0) for Neo robot
 	void setOffset(Pose3D off){offset=off;}
@@ -32,8 +35,13 @@ public:
 
 	Pose3D getEstimatedPose(){return estimatedPose;}
 	
-	Path3D odomTraj,groundTraj,filterTraj;
+//	Path3D odomTraj,groundTraj,filterTraj;
 	void printInfo();
+	void printParams(){
+		LOG_INFO("Neff: "<<neffThreshold);
+		LOG_INFO("Num: "<<particles.size());
+		LOG_INFO("SkipLaser: "<<skipLaser);
+	}
 
 private:
 	void computeDrawWeights();
@@ -48,8 +56,10 @@ private:
 	Pose3D odomPose;
 	Pose3D estimatedPose;
 	Pose3D ground;
-	//Path3D odomTraj,groundTraj,filterTraj;
+	Path3D odomTraj,groundTraj,filterTraj;
 	Pose3D offset;
 	LaserData laserD;
 	float neffThreshold;
+	int skipLaser;//still not used, must be integrated in worldSim
+	WheeledBaseSim* base; 
 };
